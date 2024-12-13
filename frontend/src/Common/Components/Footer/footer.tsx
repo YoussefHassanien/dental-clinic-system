d91aaf1031f4969d168a1196618c5bde39cdbe4d
Youssef Hassanien <Youssefhassanein17@gmail.com>
2024-12-13 19:30:41 +0200

    Added some icons to the footer Home Page Frontend Done

--- frontend/src/Common/Components/Footer/footer.tsx
+++ frontend/src/Common/Components/Footer/footer.tsx
@@ -1,3 +1,7 @@
+import facebookIcon from "../../../assets/Facebook.svg";
+import linkedinIcon from "../../../assets/LinkedIn.svg";
+import twitterIcon from "../../../assets/Twitter.svg";
+import instagramIcon from "../../../assets/Instagram.svg";
 import { Link } from "react-router-dom";
 
 const Footer: React.FC = () => {
@@ -18,12 +22,50 @@ const Footer: React.FC = () => {
   return (
     <footer className="bg-gray-800 text-white py-8 w-full font-serif">
       <div className="container mx-auto flex flex-col md:flex-row justify-between items-center">
-        <div className="mb-4 md:mb-0">
+        <div className="mb-4 md:mb-0 space-y-1">
           <h2 className="text-2xl font-bold">DentiPlus Clinics</h2>
           <p className="text-sm">
             Providing exceptional dental care since{" "}
             <span className="font-sans">1992</span>
           </p>
+          <div className="mt-4 md:mt-0 flex space-x-4">
+            <a
+              href="https://www.facebook.com"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="text-white hover:text-gray-400"
+            >
+              <img src={facebookIcon} alt="facebook-icon" className="w-6 h-6" />
+            </a>
+            <a
+              href="https://www.linkedin.com"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="text-white hover:text-gray-400"
+            >
+              <img src={linkedinIcon} alt="linkedin-icon" className="w-6 h-6" />
+            </a>
+            <a
+              href="https://www.twitter.com"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="text-white hover:text-gray-400"
+            >
+              <img src={twitterIcon} alt="twitter-icon" className="w-6 h-6" />
+            </a>
+            <a
+              href="https://www.instagram.com"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="text-white hover:text-gray-400"
+            >
+              <img
+                src={instagramIcon}
+                alt="instagram-icon"
+                className="w-6 h-6"
+              />
+            </a>
+          </div>
         </div>
         <div className="flex space-x-4">
           <a
