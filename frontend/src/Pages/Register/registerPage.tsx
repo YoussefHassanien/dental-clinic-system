faaeaa95562c6459df1d6e0c002a680868892bbc
Youssef Hassanien <Youssefhassanein17@gmail.com>
2024-12-14 16:20:03 +0200

    Added password visibility toggle

--- frontend/src/Pages/Register/registerPage.tsx
+++ frontend/src/Pages/Register/registerPage.tsx
@@ -10,6 +10,11 @@ const RegisterPage: React.FC = () => {
   const [month, setMonth] = useState("Month");
   const [year, setYear] = useState("Year");
   const [gender, setGender] = useState("");
+  const [isPasswordVisible, setIsPasswordVisible] = useState(false);
+  const [password, setPassword] = useState("");
+  const [isConfirmPasswordVisible, setIsConfrimPasswordVisible] =
+    useState(false);
+  const [confirmPassword, setConfirmPassword] = useState("");
 
   return (
     <div className="h-fit flex flex-col justify-between items-center">
@@ -154,20 +159,108 @@ const RegisterPage: React.FC = () => {
             />
           </div>
           <div className="flex flex-row space-x-4 p-2 font-serif items-center">
-            <input
-              type="password"
-              id="password"
-              className="text-gray-400 rounded-md border-2 p-2 w-full"
-              placeholder="Password"
-              required={true}
-            />
-            <input
-              type="password"
-              id="confirmPassword"
-              className="text-gray-400 rounded-md border-2 p-2 w-full"
-              placeholder="Confirm Password"
-              required={true}
-            />
+            <div className="flex flex-row justify-between py-2 font-serif items-center text-gray-400 w-full space-x-2">
+              <input
+                type={isPasswordVisible ? "text" : "password"}
+                id="registerPassword"
+                placeholder="Password"
+                required={true}
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                className="w-full rounded-md border-2 p-2"
+              />
+              {isPasswordVisible ? (
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  strokeWidth={1.5}
+                  stroke="currentColor"
+                  className="size-6 cursor-pointer"
+                  onClick={() => setIsPasswordVisible(!isPasswordVisible)}
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M2.036 12.322a1.012 1.012 0 0 1 0-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178Z"
+                  />
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M15 12a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z"
+                  />
+                </svg>
+              ) : (
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  strokeWidth={1.5}
+                  stroke="currentColor"
+                  className="size-6 cursor-pointer"
+                  onClick={() => setIsPasswordVisible(!isPasswordVisible)}
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M3.98 8.223A10.477 10.477 0 0 0 1.934 12C3.226 16.338 7.244 19.5 12 19.5c.993 0 1.953-.138 2.863-.395M6.228 6.228A10.451 10.451 0 0 1 12 4.5c4.756 0 8.773 3.162 10.065 7.498a10.522 10.522 0 0 1-4.293 5.774M6.228 6.228 3 3m3.228 3.228 3.65 3.65m7.894 7.894L21 21m-3.228-3.228-3.65-3.65m0 0a3 3 0 1 0-4.243-4.243m4.242 4.242L9.88 9.88"
+                  />
+                </svg>
+              )}
+            </div>
+            <div className="flex flex-row justify-between py-2 font-serif items-center text-gray-400 w-full space-x-2">
+              <input
+                type={isConfirmPasswordVisible ? "text" : "password"}
+                id="regterConfirmPassword"
+                placeholder="Confirm Password"
+                required={true}
+                value={confirmPassword}
+                onChange={(e) => setConfirmPassword(e.target.value)}
+                className="w-full rounded-md border-2 p-2"
+              />
+              {isConfirmPasswordVisible ? (
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  strokeWidth={1.5}
+                  stroke="currentColor"
+                  className="size-6 cursor-pointer"
+                  onClick={() =>
+                    setIsConfrimPasswordVisible(!isConfirmPasswordVisible)
+                  }
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M2.036 12.322a1.012 1.012 0 0 1 0-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178Z"
+                  />
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M15 12a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z"
+                  />
+                </svg>
+              ) : (
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  strokeWidth={1.5}
+                  stroke="currentColor"
+                  className="size-6 cursor-pointer"
+                  onClick={() =>
+                    setIsConfrimPasswordVisible(!isConfirmPasswordVisible)
+                  }
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M3.98 8.223A10.477 10.477 0 0 0 1.934 12C3.226 16.338 7.244 19.5 12 19.5c.993 0 1.953-.138 2.863-.395M6.228 6.228A10.451 10.451 0 0 1 12 4.5c4.756 0 8.773 3.162 10.065 7.498a10.522 10.522 0 0 1-4.293 5.774M6.228 6.228 3 3m3.228 3.228 3.65 3.65m7.894 7.894L21 21m-3.228-3.228-3.65-3.65m0 0a3 3 0 1 0-4.243-4.243m4.242 4.242L9.88 9.88"
+                  />
+                </svg>
+              )}
+            </div>
           </div>
 
           <hr className="bg-gray-500 my-2 w-full" />
