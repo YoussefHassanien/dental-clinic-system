df9cb8b5416dbec9c49bce6f99fd114b612888a5
omarrrefaatt <119802537+omarrrefaatt@users.noreply.github.com>
2024-12-30 15:50:46 +0200

    chatbot done

--- backend/server.js
+++ backend/server.js
@@ -21,6 +21,7 @@ const appointmentRoute = require("./routes/appointmentRoute");
 const documentRoute = require("./routes/documnetRoute");
 const requestRoute = require("./routes/requestRoute");
 const statisticsRoute = require("./routes/statisticsRoute");
+const chatbotRoute = require("./routes/chatbotRoute");
 // database configuration
 dbconnection();
 
@@ -50,6 +51,7 @@ app.use("/api/v1/appointments", appointmentRoute);
 app.use("/api/v1/documents", documentRoute);
 app.use("/api/v1/requests", requestRoute);
 app.use("/api/v1/statistics", statisticsRoute);
+app.use("/api/v1/chatbot", chatbotRoute);
 
 // handle unvalid routes
 app.all("*", (req, res, next) => {
